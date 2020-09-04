5170b5531851cf5d944855ef9f03387dd4d63bca
Guillaume Chalons <iambluedev@gmx.fr>
2020-09-04 19:33:26 +0200

    fix(exo-4) : ajout de commentaires

--- tp-1/exo-4/détaillée.c
+++ tp-1/exo-4/détaillée.c
@@ -25,6 +25,13 @@ int main()
 
 int demander(void) {
     int value = 0;
+    //On est pas obligé de définir un corps à la boucle while (si on en a pas besoin), ici cela revient à ecrire while(scanf("%d", &value) == 0){}
+    //On profite également de la boucle pour effectuer une affectation et une vérification comme ça on gagne des lignes
+    //Sinon on aurait du faire quelque chose comme : 
+    //  int res = 0;
+    //  while(res == 0) {
+    //      res = scanf("%d", &value);
+    //  }
     while(scanf("%d", &value) == 0);
     return value;
 }
